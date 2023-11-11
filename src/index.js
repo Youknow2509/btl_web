5830d083bcb81088615cca7fae61c1ebb5712b62
V <lytranvinh.work@gmail.com>
2023-11-12 03:16:05 +0700

    add static name and partials

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
-const { create }  = require('express-handlebars');
+const { engine }  = require('express-handlebars');
 const fs = require('fs');
 const path = require('path');
 const port = 3000;
 
 const app = express();
-const hbs = create({
-    /* config */ 
-});
 // create a write stream (in append mode)
 const accessLogStream = fs.createWriteStream(path.join(path.join(__dirname, '..'), 'access.log'), { flags: 'a' });
 // setup the logger
@@ -16,17 +13,21 @@ app.use(
     morgan('combined', { stream: accessLogStream })
 );
 // templace engine "EXPRESS-HANDLEBARS"
-app.engine('handlebars', hbs.engine);
+app.engine('hbs', engine({extname: '.hbs'}));
 // Register `app.handlebars` with the Express app.
-app.set('view handlebars', 'handlebars');
+app.set('view handlebars', '.hbs');
 app.set('views', path.join(__dirname,'resource/views'));
 // route 
 app.get('/', (req, res) => {
     res.render(
-        'home.handlebars'
+        'home.hbs'
     );
 });
-
+app.get('/news', (req, res) => {
+    res.render(
+        'news.hbs'
+    );
+}); 
 app.listen(port, () => {
     console.log(`Example app listening on http://localhost:${port}`);
 });
