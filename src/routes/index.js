634a02800ff48b780546d0fc9c09623e74208afe
V <lytranvinh.work@gmail.com>
2023-11-15 01:26:24 +0700

    fix router

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,19 +4,6 @@ const siteRouter = require('./site');
 function route(app) {
     app.use('/news', newRouter);
     app.use('/', siteRouter);
-
-    app.get('/', (req, res) => {
-        res.render('home.hbs');
-    });
-
-    app.get('/search', (req, res) => {
-        console.log(req.query);
-        res.render('search.hbs');
-    });
-
-    app.post('/search', (req, res) => {
-        res.render('search.hbs');
-    });
 }
 
 module.exports = route;
